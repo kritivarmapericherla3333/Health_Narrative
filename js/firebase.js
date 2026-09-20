8036712f59233f9fb3ebdfb46e11d2e789b9f909
kritivarmapericherla3333 <kritivarmapericherla3333@gmail.com>
2026-09-20 21:44:16 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,42 +1,22 @@
-import { initializeApp }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
-
-getDatabase,
-ref,
-get
-
-}
-
-from
-
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-apiKey: "AIzaSyBRFfsMtyzwG0xbLI9k90perHJeSdJPAhU",
-
-authDomain: "healthnarrative-c4af3.firebaseapp.com",
-
-databaseURL:
-"https://healthnarrative-c4af3-default-rtdb.firebaseio.com",
-
-projectId: "healthnarrative-c4af3",
-
-storageBucket:
-"healthnarrative-c4af3.firebasestorage.app",
-
-messagingSenderId: "637852726804",
-
-appId:
-"1:637852726804:web:ea41a628ff1cef57341901"
-
+  apiKey: "AIzaSyBRFfsMtyzwG0xbLI9k90perHJeSdJPAhU",
+  authDomain: "healthnarrative-c4af3.firebaseapp.com",
+  databaseURL: "https://healthnarrative-c4af3-default-rtdb.firebaseio.com",
+  projectId: "healthnarrative-c4af3",
+  storageBucket: "healthnarrative-c4af3.firebasestorage.app",
+  messagingSenderId: "637852726804",
+  appId: "1:637852726804:web:ea41a628ff1cef57341901",
+  measurementId: "G-WJSTPR6L08"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const database = getDatabase(app);
-
-export {database,ref,get};
+const analytics = getAnalytics(app);
